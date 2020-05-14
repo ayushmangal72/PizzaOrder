8345c45b7e3f08789787b0845bf629dcd852941a
Ayush Mangal <51397352+ayushmangal72@users.noreply.github.com>
2020-05-14 16:23:59 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manga
+ */
+public class demo1 {
+
+    void fun()
+    {
+        System.out.println("Fun Called");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        demo1 ob=new demo1();
+        ob.fun();
+        
+    }
+
+}
